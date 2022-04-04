077cabd5edd58da76523cb5c614495bcac79bd63
Владислав Цой <tsoy.1996.vlad@gmail.com>
2022-04-04 16:24:39 +0600

    Добавил hook на коммиты

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const queryClient = new QueryClient();
+const rootElem = document.getElementById('root');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root'),
+if (!rootElem) throw new Error('Failed to find the root element');
+
+createRoot(rootElem).render(
+  <Router>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </Router>,
 );
 
 // If you want to start measuring performance in your app, pass a function
